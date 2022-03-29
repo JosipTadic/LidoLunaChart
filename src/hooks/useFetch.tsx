b597ca0440a31f6c0f86a45e61ff60bda1bdefba
unknown <josiptadic96@hotmail.com>
2022-03-29 22:02:09 +0200

    useFetch small refactor

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,28 +1,25 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { CryptoList } from "../react-app-env"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { CryptoList } from "../react-app-env";
 
-export default function useFetch( url: string ){
+export default function useFetch(url: string) {
+  const [data, setData] = useState<CryptoList>();
+  const [error, setError] = useState<unknown>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const [ data, setData ] = useState<CryptoList>()
-    const [ error, setError ] = useState<unknown>(null)
-    const [ loading, setLoading] = useState<boolean>(false)
+  useEffect(() => {
+    (async function () {
+      try {
+        setLoading(true);
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
 
-    useEffect(() => {
-        (
-            async function(){
-                try{
-                    setLoading(true)
-                    const response = await axios.get(url)
-                    setData(response.data)
-                }catch(err){
-                    setError(err)
-                }finally{
-                    setLoading(false)
-                }
-            }
-        )()
-    }, [url])
-
-    return { data, error, loading }
+  return { data, error, loading };
 }
